4ea3e0f802e5e47ce1382535ddbda473cb30f135
jacky <awaisjabri6@gmail.com>
2026-02-05 17:36:06 +0530

    Update outputs.tf

--- ansible-challenge/terraform/outputs.tf
+++ ansible-challenge/terraform/outputs.tf
@@ -1,11 +1,3 @@
-resource "local_file" "inventory" {
-  content = templatefile("${path.module}/inventory.tpl", {
-    frontend_ip = aws_instance.frontend.public_ip
-    backend_ip  = aws_instance.backend.public_ip
-  })
-  filename = "./../ansible/inventory.ini"
-}
-
 output "inventory" {
   value = local_file.inventory.content
 }
