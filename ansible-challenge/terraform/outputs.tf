052086bb44ccf599329d1d3c566fe4b231fe8af5
jacky <awaisjabri6@gmail.com>
2026-02-05 17:33:05 +0530

    Update outputs.tf

--- ansible-challenge/terraform/outputs.tf
+++ ansible-challenge/terraform/outputs.tf
@@ -4,4 +4,7 @@ frontend_ip = aws_instance.frontend.public_ip
 backend_ip = aws_instance.backend.public_ip
 })
 filename = "${path.module}/../ansible/inventory"
+output "inventory" {
+  value = local_file.inventory.content
+}
 }
